29da3692da04e2ba5b5d839ceb0126b9c990f105
nick-a-schneider <schneidernick3@gmail.com>
2021-05-11 14:48:47 -0400

    updated macros

--- Wheel_Encoder_ROS_Integration_w_interrupts/include/macros.h
+++ Wheel_Encoder_ROS_Integration_w_interrupts/include/macros.h
@@ -7,61 +7,14 @@
 ///////////////
 //Macro Pinouts for  Front Left Wheel Encoder Chip
 ///////////////
-#define LEFT_SIDE_DIR 34
+#define LEFT_SIDE_DIR  5
+#define L_HALL_CHANNEL 0
+#define L_MOTOR_PWM 11
 
 
-#define L_HALL_CHANNEL 2
-
-#define L_MOTOR_PWM 10
-#define FL_CURRENTSENSE 24
-
-//////////////
-//Macro Pinouts for  Mid Left Wheel Encoder Chip
-//////////////
-
-#define ML_HALL_CHANNEL 16
-
-#define ML_MOTOR_PWM 11
-#define M_CURRENTSENSE 41
-
-/////////////
-//Macros Pinouts for  Back Left Wheel Encoder Chip
-/////////////
-
-#define BL_HALL_CHANNEL   25
-
-#define BL_MOTOR_PWM 33
-#define BL_CURRENTSENSE 40
-
-///////////////
-//Macro Pinouts for  Front Right Wheel Encoder Chip
-///////////////
-#define RIGHT_SIDE_DIR 0
-
-
+#define RIGHT_SIDE_DIR 8
 #define R_HALL_CHANNEL 3
-
-#define R_MOTOR_PWM 8
-#define FR_CURRENTSENSE 
-
-//////////////
-//Macro Pinouts for  Mid Left Wheel Encoder Chip
-//////////////
-
-#define MR_HALL_CHANNEL 4
-
-#define MR_MOTOR_PWM 7
-#define MR_CURRENTSENSE 
-
-/////////////
-//Macros Pinouts for  Back Left Wheel Encoder Chip
-/////////////
-
-#define BR_HALL_CHANNEL 5
-
-#define BR_MOTOR_PWM 6
-#define B_CURRENTSENSE 
-
+#define R_MOTOR_PWM 1
 //////////////
 //Macros for ROS
 //////////////
